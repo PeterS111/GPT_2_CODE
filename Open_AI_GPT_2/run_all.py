dbf525831047db93d434ef8f12d2815d142c16a2
PeterS111 <44571558+PeterS111@users.noreply.github.com>
2022-06-11 08:19:23 +0100

    Update run_all.py

--- Open_AI_GPT_2/run_all.py
+++ Open_AI_GPT_2/run_all.py
@@ -29,7 +29,7 @@ sample_length = 600
 model_descr = "Shelley"
 
 # This directory_path is valid only for Colab notebooks working with our repository:
-directory_path = "/content/Open_AI_GPT_2_fine_tune_and_generate/"
+directory_path = "/content/GPT_2_CODE/Open_AI_GPT_2/"
 
 ####################################
 # Text prompt for the samples
@@ -52,10 +52,10 @@ for i in range(1,11):
     # You can choose to fine-tune the model with or without validation. Comment out as required:
     
     # Fine-tuning the model without validatation:
-    os.system('python train.py --dataset {} --model_name {} --top_k 50 --top_p 1.0 --max_steps {} --save_every {} --sample_every 100000 --max_to_keep 10  --val_batch_count 40'.format(input_name, model_size, ckpt, steps_increment))
+    os.system('python train.py --dataset {} --model_name {} --top_k 50 --top_p 1.0 --max_steps {} --save_every {}'.format(input_name, model_size, ckpt, steps_increment))
     
     # Fine-tuning the model with validatation:
-    # os.system('python train.py --dataset {} --val_dataset {} --val_every 50 --model_name {} --top_k 50 --top_p 1.0 --max_steps {} --save_every {} --max_to_keep 10  --val_batch_count 40'.format(input_train, input_val, model_size, ckpt, ckpt))
+    # os.system('python train.py --dataset {} --val_dataset {} --val_every 50 --model_name {} --top_k 50 --top_p 1.0 --max_steps {} --save_every {}'.format(input_train, input_val, model_size, ckpt, ckpt))
  
     # This line calls the "mover.py" script to copy the fine-tuned model into the "models" folder
     # and to copy from the original model the files required to run the fine-tuned model:
@@ -64,7 +64,7 @@ for i in range(1,11):
     # Generation loop:
     for s in range(seed_start, seed_end):
         # Change the --raw_text, --top_k, --top_p and --temperature arguments as required:
-        os.system('python generate_conditional_samples_to_file.py --raw_text {prompt} --model_descr {} --model_name model_{} --length {} --seed {} --temperature 1.0 --top_k 50 --top_p 1.0 --nsamples 1'.format(prompt, model_descr, ckpt, sample_length, s))
+        os.system('python generate_conditional_samples_to_file.py --raw_text {prompt} --model_descr {} --model_name model_{} --length {} --seed {} --temperature 1.0 --top_k 50 --top_p 1.0'.format(prompt, model_descr, ckpt, sample_length, s))
 
     # This code will remove the fine-tuned models that finished generation:
     # (comment out if you want to keep them):
