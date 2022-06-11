6cfd8564312b236851467759f05e1ba2b63ce809
PeterS111 <44571558+PeterS111@users.noreply.github.com>
2022-06-11 08:28:13 +0100

    Update run_all.py

--- Open_AI_GPT_2/run_all.py
+++ Open_AI_GPT_2/run_all.py
@@ -1,34 +1,35 @@
-# This code was used to train the models and generate samples from OpenAI GPT-2 models used in our paper:
-# 'Training GPT-2 to represent two Romantic-era authors challenges, evaluations and pitfalls'.
+## This code was used to train the models and generate samples from OpenAI GPT-2 models used in our paper:
+## 'Training GPT-2 to represent two Romantic-era authors challenges, evaluations and pitfalls'.
 
 import os
 import shutil
 import argparse
 
 
-# model_size is passed as an argument from the colab notebook:
+## model_size is passed as an argument from the colab notebook:
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_size", type=str, default="124M")
+args = parser.parse_args()
 model_size = args.model_size
 
-# seed_start and seed_end set the range of samples being generated from your models:
+## seed_start and seed_end set the range of samples being generated from your models:
 seed_start = 1
 seed_end = 1001
 
-# ckpt sets the first checkpoint where the model will be saved and the samples will be generated from:
+## ckpt sets the first checkpoint where the model will be saved and the samples will be generated from:
 ckpt = 1000
 
-#steps_increment sets the increment of steps between checkpoints that are saved and generated from:
+## steps_increment sets the increment of steps between checkpoints that are saved and generated from:
 steps_increment = 1000
 
-# sample_length sets the length of the samples generated (maximum is 1024, but if you are using prompt, 
-# you will have to deduct the prompt size from this number):
+## sample_length sets the length of the samples generated (maximum is 1024, but if you are using prompt, 
+## you will have to deduct the prompt size from this number):
 sample_length = 600
 
-# model_descr can be anything, it is used only to name your samples: 
+## model_descr can be anything, it is used only to name your samples: 
 model_descr = "Shelley"
 
-# This directory_path is valid only for Colab notebooks working with our repository:
+##This directory_path is valid only for Colab notebooks working with our repository:
 directory_path = "/content/GPT_2_CODE/Open_AI_GPT_2/"
 
 ####################################
@@ -37,8 +38,7 @@ prompt = "The eternal sky "
 ####################################
 prompt = prompt.replace(" ", "$$$$$")
 
-
-# Dataset for training without validation:
+## Dataset for training without validation:
 input_name = "input_data/Shelley.txt"
 
 ## Datasets for training with validation:
@@ -64,7 +64,7 @@ for i in range(1,11):
     # Generation loop:
     for s in range(seed_start, seed_end):
         # Change the --raw_text, --top_k, --top_p and --temperature arguments as required:
-        os.system('python generate_conditional_samples_to_file.py --raw_text {prompt} --model_descr {} --model_name model_{} --length {} --seed {} --temperature 1.0 --top_k 50 --top_p 1.0'.format(prompt, model_descr, ckpt, sample_length, s))
+        os.system('python generate_conditional_samples_to_file.py --raw_text {} --model_descr {} --model_name model_{} --length {} --seed {} --temperature 1.0 --top_k 50 --top_p 1.0'.format(prompt, model_descr, ckpt, sample_length, s))
 
     # This code will remove the fine-tuned models that finished generation:
     # (comment out if you want to keep them):
